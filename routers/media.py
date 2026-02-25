958f6ded356ee637c4120689cbc1fc47bd68f3aa
RasulbekGit <rasuliztileuov@gmail.com>
2026-02-25 18:43:56 +0500

    add commands image. doc

--- routers/media.py
+++ routers/media.py
@@ -1,8 +1,41 @@
-from aiogram import Router, F, types
+from aiogram import F, Router, types
+from aiogram.enums.chat_action import ChatAction
+from aiogram.filters import Command
+from aiogram.types import FSInputFile
 
 router = Router()
 
 
 @router.message(F.photo)
-async def send_message_photo(message: types.Message):
-    await message.answer("I cant view photo")
+async def photo_handler(message: types.Message):
+    await message.answer("I cant view photo!")
+
+
+@router.message(Command("image"))
+async def image_cmd(message: types.Message):
+    # img_url = (
+    #     "https://cs14.pikabu.ru/post_img/2023/10/10/6/og_og_1696928113214795976.jpg"
+    # )
+    cat_image_path = "C:\\Users\\Asus\\Pictures\\photo_2025-06-03_17-08-19.jpg"
+    image = FSInputFile(cat_image_path)
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_PHOTO,
+    )
+    await message.answer_photo(
+        photo=image,
+        caption="my photo",
+    )
+
+
+@router.message(Command("doc"))
+async def document_cmd(message: types.Message):
+    doc = FSInputFile("C:\\Users\\Asus\\Documents\\hello.txt")
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_DOCUMENT,
+    )
+    await message.answer_document(
+        document=doc,
+        caption="my document",
+    )
